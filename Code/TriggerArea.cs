e360bea16dc781dc706f9d8bf082f3878c667a85
Thunraz <thunraz@thunraz.com>
2014-04-26 12:59:12 +0000

    Added functionality to TriggerArea so it can be triggered only once.

--- Code/TriggerArea.cs
+++ Code/TriggerArea.cs
@@ -14,18 +14,26 @@ namespace JamTemplate
     {
         private FloatRect _rect;
 
+        public bool HasTriggered { get; private set; }
+        public bool IsTriggeredOnce { get; private set; }
         public string Id { get; private set; }
         public TriggerAreaType Type { get; private set; }
 
-        public TriggerArea(FloatRect rect, TriggerAreaType type, string id)
+        public TriggerArea(FloatRect rect, TriggerAreaType type, string id, bool isTriggeredOnce = false)
         {
             _rect = rect;
+            IsTriggeredOnce = isTriggeredOnce;
+            HasTriggered = false;
             Type = type;
             Id = id;
         }
 
         public bool CheckIsInside(Vector2f pos)
         {
+            if (IsTriggeredOnce && HasTriggered)
+                return false;
+
+            HasTriggered = true;
             return _rect.Contains(pos.X, pos.Y);
         }
     }
